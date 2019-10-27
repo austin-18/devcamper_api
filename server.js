d4b5ed55af43e3974f481ace82cbf53edc56315f
Austin Bracey <austin.bracey18@gmail.com>
2019-10-27 14:41:04 -0500

    added connection and implementation of MongoDB with mongoose

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors'); // colors library to color text in to console (this is optional)
 //const logger = require('./middleware/logger');
 const morgan = require('morgan');
-
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
-
+// importing connectDB method from db.js to connect to MongoDB
+const connectDB = require('./config/db');
 
 // Load env variables
 dotenv.config({ path: './config/config.env'});
+
+// connectDB() method must be somewhere under the line that loading the environment variables config file (lines above)
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -26,7 +30,15 @@ app.use('/api/v1/bootcamps', bootcamps); //mounts the bootcamps routes from boot
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
 
-
+// Handling unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // close server and exit process
+    server.close(() => process.exit(1));
+});
 
