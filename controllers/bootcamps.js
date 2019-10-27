0afadbd4200006bae20b51394a4f7bb85eb779a5
Austin Bracey <austin.bracey18@gmail.com>
2019-10-26 21:06:09 -0500

    Added controller folder for functinality and updated routes folder

---
+++ controllers/bootcamps.js
@@ -0,0 +1,49 @@
+// @desc:    Get all bootcamps
+// @route:   GET /api/v1/bootcamps
+// @access:  Public
+exports.getBootcamps = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: 'show all bootcamps'
+    });
+}
+
+// @desc:    Get bootcamp bty ID
+// @route:   GET /api/v1/bootcamps/:id
+// @access:  Public
+exports.getBootcamp = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: `show bootcamp ${req.params.id}`
+    });
+}
+
+// @desc:    create new bootcamp
+// @route:   POST /api/v1/bootcamps
+// @access:  Private
+exports.createBootcamp = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: 'create new bootcamp'
+    });
+}
+
+// @desc:    update bootcamp by ID
+// @route:   PUT /api/v1/bootcamps/:id
+// @access:  Private
+exports.updateBootcamp = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: `update bootcamp ${req.params.id}`
+    });
+}
+
+// @desc:    delete bootcamp by Id
+// @route:   DELETE /api/v1/bootcamps
+// @access:  Private
+exports.deleteBootcamp = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: `delete bootcamp ${req.params.id}`
+    });
+}
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,41 +1,23 @@
 const express = require('express');
+const { 
+    getBootcamps, 
+    getBootcamp, 
+    createBootcamp, 
+    updateBootcamp, 
+    deleteBootcamp
+} = require('../controllers/bootcamps');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {  // get all bootcamps
-    // res.send('Hello'); // can put html, text, json in .send() - the header will automatically display the type for us
-    // res.status(400).json({success: false});
-    res.status(200).json({
-        success: true,
-        msg: 'show all bootcamps'
-    });
-});
+router
+    .route('/')
+    .get(getBootcamps)
+    .post(createBootcamp);
 
-router.get('/:id', (req, res) => { // get single bootcamp using the bootcamp ID (eg. /bootcamp/1)
-    res.status(200).json({
-        success: true,
-        msg: `show bootcamp ${req.params.id}`
-    });
-});
-
-router.post('/', (req, res) => {  // create a new bootcamp
-    res.status(200).json({
-        success: true,
-        msg: 'create new bootcamp'
-    });
-});
-
-router.put('/:id', (req, res) => { // delete a bootcamp by ID
-    res.status(200).json({
-        success: true,
-        msg: `update bootcamp ${req.params.id}`
-    });
-});
-
-router.delete('/:id', (req, res) => { 
-    res.status(200).json({
-        success: true,
-        msg: `delete bootcamp ${req.params.id}`
-    });
-});
+router
+    .route('/:id')
+    .get(getBootcamp)
+    .put(updateBootcamp)
+    .delete(deleteBootcamp);
 
 module.exports = router;
