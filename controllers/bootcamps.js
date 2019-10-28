f7f326955acb6e93290c0dff457e68cb8f59d9b9
Austin Bracey <austin.bracey18@gmail.com>
2019-10-27 19:59:29 -0500

    Added all API requests to GET/POST/PUT/DELETE bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,27 +9,58 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc:    Get all bootcamps
 // @route:   GET /api/v1/bootcamps
 // @access:  Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: 'show all bootcamps'
-    });
-}
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({
+            success: true,
+            count: bootcamps.length,
+            data: bootcamps
+        });
+
+    } catch (error) {
+        // setting status to 400 if an error occurs and setting success to false
+        res.status(400).json({
+            success: false
+        });
+    }
+};
 
 // @desc:    Get bootcamp bty ID
 // @route:   GET /api/v1/bootcamps/:id
 // @access:  Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `show bootcamp ${req.params.id}`
-    });
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        // IF statement will catch the if the ID that is POSTed doesnt exist in the database, but it is correct format
+        if(!bootcamp) {
+            return res.status(400).json({ //
+                success: false
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        });
+
+    } catch (error) {
+        // setting status to 400 if an error occurs and setting success to false
+        // this will catch if the format of the POSTed ID is incorrect, but will miss
+        //      if the ID is the correct format but doesnt actually exist in the database
+        //      The IF statement above catches correct format but non-existant ID
+        res.status(400).json({
+            success: false
+        });
+    }
 }
 
 // @desc:    create new bootcamp
 // @route:   POST /api/v1/bootcamps
 // @access:  Private
-exports.createBootcamp = async(req, res, next) => {    
+exports.createBootcamp = async (req, res, next) => {    
     try {
         const bootcamp = await Bootcamp.create(req.body); // when using async/await, must create new variable to access data from whatever is awaiting
 
@@ -44,24 +75,59 @@ exports.createBootcamp = async(req, res, next) => {
             success: false
         });
     }
-}
+};
 
 // @desc:    update bootcamp by ID
 // @route:   PUT /api/v1/bootcamps/:id
 // @access:  Private
-exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `update bootcamp ${req.params.id}`
-    });
-}
+exports.updateBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        // IF statement will catch the if the ID that is DELETEed doesnt exist in the database, but it is correct format
+        if(!bootcamp) {
+            return res.status(400).json({ //
+                success: false
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            success: false
+        });
+    }
+};
 
 // @desc:    delete bootcamp by Id
 // @route:   DELETE /api/v1/bootcamps
 // @access:  Private
-exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `delete bootcamp ${req.params.id}`
-    });
-}
+exports.deleteBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+        // IF statement will catch the if the ID that is DELETEed doesnt exist in the database, but it is correct format
+        if(!bootcamp) {
+            return res.status(400).json({ //
+                success: false
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            success: false
+        });
+    }
+};
