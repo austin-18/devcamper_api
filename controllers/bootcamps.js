f8c58b44f885d48a56fdc46e39b71fe728e93f25
Austin Bracey <austin.bracey18@gmail.com>
2019-10-27 21:30:41 -0500

    Created asyncHandler middleware to clean up our controllers

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,8 +1,10 @@
 // NOTE: All model files are to begin with an uppercase letter. 
 //       All controller files are to begin with a lowercase letter
 
-// 
+// Importing ErrorResponse Class (contains a constructor)
 const ErrorResponse = require('../utils/errorResponse');
+// importing the asyncHandler middleware that will eliminate the need for try/catch blocks with next() methods
+const asyncHandler = require('../middleware/async');
 // importing the model into the controller
 const Bootcamp = require('../models/Bootcamp');
 
@@ -13,130 +15,86 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc:    Get all bootcamps
 // @route:   GET /api/v1/bootcamps
 // @access:  Public
-exports.getBootcamps = async (req, res, next) => {
-    try {
-        const bootcamps = await Bootcamp.find();
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    const bootcamps = await Bootcamp.find();
 
-        res.status(200).json({
-            success: true,
-            count: bootcamps.length,
-            data: bootcamps
+    res.status(200).json({
+        success: true,
+        count: bootcamps.length,
+        data: bootcamps
         });
-
-    } catch (error) {
-        // setting status to 400 if an error occurs and setting success to false
-        // res.status(400).json({
-        //     success: false
-        // });
-        next(error);
-    }
-};
+    });
 
 // @desc:    Get bootcamp bty ID
 // @route:   GET /api/v1/bootcamps/:id
 // @access:  Public
-exports.getBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findById(req.params.id);
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findById(req.params.id);
 
-        // IF statement will catch the if the ID that is POSTed doesnt exist in the database, but it is correct format
-        if(!bootcamp) {
-            return next(
-                new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-            );
-        }
-
-        res.status(200).json({
-            success: true,
-            data: bootcamp
-        });
-
-    } catch (error) {
-        // setting status to 400 if an error occurs and setting success to false
-        // this will catch if the format of the POSTed ID is incorrect, but will miss
-        //      if the ID is the correct format but doesnt actually exist in the database
-        //      The IF statement above catches correct format but non-existant ID
-        // res.status(400).json({
-        //     success: false
-        // });
-        next(error);
+    // IF statement will catch the if the ID that is POSTed doesnt exist in the database, but it is correct format
+    if(!bootcamp) {
+        return next(
+            new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+        );
     }
-};
+
+    res.status(200).json({
+        success: true,
+        data: bootcamp
+    });
+});
 
 // @desc:    create new bootcamp
 // @route:   POST /api/v1/bootcamps
 // @access:  Private
-exports.createBootcamp = async (req, res, next) => {    
-    try {
-        const bootcamp = await Bootcamp.create(req.body); // when using async/await, must create new variable to access data from whatever is awaiting
+exports.createBootcamp = asyncHandler(async (req, res, next) => {    
+    const bootcamp = await Bootcamp.create(req.body); // when using async/await, must create new variable to access data from whatever is awaiting
 
-        // setting status as 201 because we're creating new data
-        res.status(201).json({
-            success: true,
-            data: bootcamp
-        });
-    } catch (error) {
-        // setting status to 400 if an error occurs and setting success to false
-        // res.status(400).json({
-        //     success: false
-        // });
-        next(error);
-    }
-};
+    // setting status as 201 because we're creating new data
+    res.status(201).json({
+        success: true,
+        data: bootcamp
+    });
+
+});
 
 // @desc:    update bootcamp by ID
 // @route:   PUT /api/v1/bootcamps/:id
 // @access:  Private
-exports.updateBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
 
-        // IF statement will catch the if the ID that is DELETEed doesnt exist in the database, but it is correct format
-        if(!bootcamp) {
-            return next(
-                new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-            );
-        }
-
-        res.status(200).json({
-            success: true,
-            data: bootcamp
-        });
-
-    } catch (error) {
-        // res.status(400).json({
-        //     success: false
-        // });
-        next(error);
+    // IF statement will catch the if the ID that is DELETEed doesnt exist in the database, but it is correct format
+    if(!bootcamp) {
+        return next(
+            new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+        );
     }
-};
+
+    res.status(200).json({
+        success: true,
+        data: bootcamp
+    });
+});
 
 // @desc:    delete bootcamp by Id
 // @route:   DELETE /api/v1/bootcamps
 // @access:  Private
-exports.deleteBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
-        // IF statement will catch the if the ID that is DELETEed doesnt exist in the database, but it is correct format
-        if(!bootcamp) {
-            return next(
-                new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-            );
-        }
-
-        res.status(200).json({
-            success: true,
-            data: {}
-        });
-
-    } catch (error) {
-        // res.status(400).json({
-        //     success: false
-        // });
-        next(error);
+    // IF statement will catch the if the ID that is DELETEed doesnt exist in the database, but it is correct format
+    if(!bootcamp) {
+        return next(
+            new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+        );
     }
-};
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+});
