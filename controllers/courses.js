7c5e375ca02e3417b60b7bea96a95aaf3b18d737
Austin Bracey <austin.bracey18@gmail.com>
2019-11-03 15:39:28 -0600

    added routes and controllesr to get courses by ID and add a new course to an existing bootcamp

--- controllers/courses.js
+++ controllers/courses.js
@@ -5,8 +5,9 @@
 const ErrorResponse = require('../utils/errorResponse');
 // importing the asyncHandler middleware that will eliminate the need for try/catch blocks with next() methods
 const asyncHandler = require('../middleware/async');
-// importing the model into the controller
+// importing the models into the controller
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc:    Get courses
 // @route:   GET /api/v1/courses
@@ -33,6 +34,44 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
         count: courses.length,
         data: courses
     });
+});
+
+// @desc:    Get single courses
+// @route:   GET /api/v1/course/:id
+// @access:  Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: 'name description'
+    });
+
+    if(!course) {
+        return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+    }
+
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+});
+
+// @desc:    Add a course
+// @route:   POST /api/v1/bootcamps/:bootcampID/courses
+// @access:  Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+    req.body.bootcamp = req.params.bootcampID;
 
 
-})
+    const bootcamp = await Bootcamp.findById(req.params.bootcampID)
+
+    if(!bootcamp) {
+        return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`), 404);
+    }
+
+    const course = await Course.create(req.body)
+
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+});
--- routes/courses.js
+++ routes/courses.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const { // importing the methods from the controller file
-    getCourses
+    getCourses,
+    getCourse,
+    addCourse
 } = require('../controllers/courses');
 
 const router = express.Router({
     mergeParams: true
 });
 
-router.route('/').get(getCourses);
+router
+    .route('/')
+    .get(getCourses)
+    .post(addCourse);
+
+router
+    .route('/:id')
+    .get(getCourse)
 
 module.exports = router;
