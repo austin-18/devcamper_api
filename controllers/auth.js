3f8f3f2d4d0dc632e6466498b172b4531b1c07e7
Austin Bracey <austin.bracey18@gmail.com>
2019-11-10 14:48:33 -0600

    Added user login and JSON web tokens to user registration and login

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,19 +6,61 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc:    Register User
-// @route:   GET /api/v1/auth/register
+// @route:   POST /api/v1/auth/register
 // @access:  Public
 exports.register = asyncHandler(async (req, res, next) => {
     const { name, email, password, role } = req.body;
 
-    // create our user
+    // create our user (this user is a static therefore we keep it lowercase)
+    // statics are derived from the model, methods are performed directly on the model
     const user = await User.create({
-        name,
-        email,
-        password,
-        role
+        name: name,
+        email: email,
+        password: password,
+        role: role
     });
 
-    res.status(200).json({success: true});
+    // Create token (static)
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({
+        success: true, 
+        token: token
+    });
+
+});
+
+// @desc:    Login User
+// @route:   POST /api/v1/auth/login
+// @access:  Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate email and password
+    if(!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+
+    // Check for user (finding by email, and then including password after finding)
+    const user = await User.findOne({ email: email }).select('+password');
+
+    if(!user){
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if(!isMatch){
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Create token (static)
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({
+        success: true, 
+        token: token
+    });
 
 });
